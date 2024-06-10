c72f3d894148078be0156c5627cd67cbfd8f06fa
Kay Mikoda <kaymikoda@MacBook-Pro-von-Kay.local>
2024-06-10 20:42:55 +0700

    Removed unwanted background pattern from global.css

---
+++ app/components/MyComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent: React.FC = () => {
+  return (
+    <div className="p-4 bg-blue-500 text-white">
+      <h1>Welcome to Smartify Logistics App</h1>
+    </div>
+  );
+};
+
+export default MyComponent;
